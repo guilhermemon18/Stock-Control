b37b637dc5c983ec6fd81fece847e0e4cd2a917b
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-07-16 16:58:32 -0300

    implementa busca de dados de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
+import { AuthGuard } from './guards/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    //lazy loading: rotas carregadas sob demanda, para evitar quedas de desempenho e atrasos no carregamento da aplicação web.
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(
+      (m) => m.DashboardModule
+    ),
+    //guarda de rotas, impede de acessar a rota usando o AuthGuard criado, validando o acesso.
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'products',
+    loadChildren: () => import('./modules/products/products.module').then(
+      (m) => m.ProductsModule
+    ),
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
