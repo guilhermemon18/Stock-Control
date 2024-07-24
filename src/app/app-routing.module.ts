f25cfa69acd1becd0c4ea9efc00cd80b27cce8a4
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-07-23 21:12:55 -0300

    Implementa componente de tabela de categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,6 +31,13 @@ const routes: Routes = [
     ),
     canActivate: [AuthGuard],
   },
+  {
+    path: 'categories',
+    loadChildren: () => import("./modules/categories/categories.module").then(
+      (m) => m.CategoriesModule
+    ),
+    canActivate: [AuthGuard],
+  }
 ];
 
 @NgModule({
