561d899aed03ceb9cd7e155747f559339a5f32d2
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-07-25 19:54:35 -0300

    Implementa funcionalidade de adicionar categoria

---
+++ src/app/modules/categories/components/category-form/category-form.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { Subject, takeUntil } from 'rxjs';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/event/EditCategoryAction';
+import { CategoryEvent } from 'src/app/models/interfaces/enums/categories/CategoryEvent';
+import { CategoriesService } from 'src/app/services/user/categories/categories.service';
+
+@Component({
+  selector: 'app-category-form',
+  templateUrl: './category-form.component.html',
+  styleUrls: []
+})
+export class CategoryFormComponent implements OnInit, OnDestroy {
+
+  private readonly destroy$: Subject<void> = new Subject();
+  private addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
+  private editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
+
+  public categoryAction!: { event: EditCategoryAction };
+  public categoryForm = this.formBuilder.group({
+    name: ['', Validators.required],
+  });
+
+  constructor(
+    public ref: DynamicDialogConfig,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private categoriesService: CategoriesService,
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  handleSubmiteAddCategory(): void {
+    if (this.categoryForm?.value && this.categoryForm?.valid) {
+      const requestCreateCategory: { name: string } = {
+        name: this.categoryForm.value.name as string,
+      };
+      this.categoriesService.createNewCategory(requestCreateCategory)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.categoryForm.reset();
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Categoria criada com sucesso',
+                life: 3000,
+              });
+            }
+          }, error: (err) => {
+            console.log(err);
+            this.categoryForm.reset();
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao criar categoria!',
+              life: 3000,
+            });
+          }
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
--- src/app/modules/categories/page/categories-home/categories-home.component.ts
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { DialogService } from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
 import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/event/DeleteCategoryAction';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { CategoriesService } from 'src/app/services/user/categories/categories.service';
+import { CategoryFormComponent } from '../../components/category-form/category-form.component';
 
 @Component({
   selector: 'app-categories-home',
@@ -16,6 +18,7 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
 
   private readonly destroy$: Subject<void> = new Subject();
   public categoriesDatas: Array<GetCategoriesResponse> = [];
+  private ref!: DynamicDialogRef;
 
   constructor(
     private categoriesService: CategoriesService,
@@ -93,6 +96,26 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
         });
 
       this.getAllCategories();
+    }
+  }
+
+  handleCategoryAction(event: EventAction): void {
+    if(event){
+      this.ref = this.dialogSevice.open(CategoryFormComponent, {
+        header: event?.action,
+        width: '70%',
+        contentStyle: { overflow: 'auto' },
+        baseZIndex: 10000,
+        maximizable: true,
+        data: {
+          event: event,
+        },
+      });
+      this.ref.onClose
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => this.getAllCategories(),
+      })
     }
   }
 
